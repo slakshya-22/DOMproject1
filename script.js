f12dc7e66c2ca9edf36299dd70e1818625f0f793
slakshya-22 <slakshyamainacc@gmail.com>
2024-12-27 20:33:30 +0530

    DOMproject1

---
+++ script.js
@@ -0,0 +1,9 @@
+const divElement = document.getElementById('myDiv');
+divElement.style.backgroundColor = 'yellow';
+divElement.style.margin = '20px';
+divElement.style.padding = '10px';
+divElement.style.fontSize = '18px';
+divElement.style.fontWeight = 'bold';
+divElement.style.height = '200px';
+divElement.style.width = '300px';
+divElement.style.color = 'blue';
